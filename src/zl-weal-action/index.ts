f0e0104d160934ef1410e85f63e5d657f7146be1
PinghuaZhuang <zphua2016@gmail.com>
2023-09-20 14:41:56 +0800

    fix: 周二福利请求接口.

--- src/zl-weal-action/index.ts
+++ src/zl-weal-action/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { notifyWithBark } from '../utils/notify';
 
-const mzUrl = '//activity.zlongame.com';
+const mzUrl = 'http://activity.zlongame.com';
 const userName = `周二福利官`;
 
 const throwError = () => {
@@ -12,27 +12,23 @@ const throwError = () => {
 };
 
 axios
-  .post(`${mzUrl}/activity/cmn/lot/wheel.do`, {
-    data: {
+  .get(`${mzUrl}/activity/cmn/lot/wheel.do`, {
+    params: {
       key: 'mz_wx_reward',
       roleid: process.env.ZL_ROLEID,
       ext1: process.env.ZL_SERVERID,
     },
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-      Referer: 'http://activity.zlongame.com/',
-    },
   })
-  .then((res) => {
-    if (res.data.success) {
-      notifyWithBark(userName, `恭喜您！抽中${res.data.info.name}`);
+  .then(({ data: { data } }) => {
+    if (data.success) {
+      notifyWithBark(userName, `恭喜您！抽中${data.info.name}`);
     } else {
-      if (res.data.code == 15) {
+      if (data.code == 15) {
         notifyWithBark(
           userName,
           `本周您已经抽取过奖励啦～\n已为您发放奖励，请前往游戏内邮箱查看`,
         );
-      } else if (res.data.code == 24) {
+      } else if (data.code == 24) {
         notifyWithBark(userName, `抽奖活动将于周二开启\n请指挥官耐心等待哦~`);
       } else {
         throwError();
