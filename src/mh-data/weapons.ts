a2bfb903df02223ff32505c23e163d958a072a3b
PinghuaZhuang <zphua2016@gmail.com>
2024-08-22 20:56:32 +0800

    pref: 优化代码.

--- src/mh-data/weapons.ts
+++ src/mh-data/weapons.ts
@@ -5,6 +5,7 @@
 import openChrome from '@/utils/openChrome';
 import { sleep } from '@/utils';
 import { BASE_URL, separator, LEVEL } from './utils';
+import { EvaluateFuncWith } from 'puppeteer';
 
 interface Weapon {
   name: string;
@@ -30,6 +31,77 @@ export function weapons2Json(weapons: Weapon[]) {
   return result;
 }
 
+const opera = (trs: HTMLTableRowElement[], separator: string) => {
+  function cleanNumber(data: string) {
+    const result = data.split('/').at(-1);
+    return result ? +result : 0;
+  }
+
+  const weaponDatas: Weapon[] = [];
+  trs.forEach((tr) => {
+    const tds = tr.querySelectorAll<HTMLTableCellElement>(`td:not(td td)`);
+    const data = Array.from(tds).map((td, index) => {
+      if (td == null) return '';
+
+      switch (index) {
+        case 0:
+          const avatar = td.querySelector(
+            'div.equip_img img',
+          ) as HTMLImageElement;
+          const aTag = td.querySelector(
+            'div.equip_name a',
+          ) as HTMLAnchorElement;
+          const bg = td.querySelector('div.equip_lv img') as HTMLImageElement;
+          return avatar
+            ? `${avatar.src}${separator}${aTag.href}${separator}${bg.src}`
+            : '';
+        case 2:
+          const img = td.querySelector('img');
+          const a = td.querySelector('a');
+          if (img == null || a == null) return '';
+          return `${img.alt.split(' ').at(-1)}${separator}${
+            img.src
+          }${separator}${a.href}`;
+        case 4:
+          return td.innerHTML.replace(/\<br\>/g, '/n').replace(/\<.*?\>/g, '');
+
+        default:
+          break;
+      }
+
+      return td.innerText;
+    });
+
+    const name = data[1].split('.')[0];
+    const heroArr = data[2].split(separator);
+    if (!name) return;
+    const tmp = data[0].split(separator);
+    weaponDatas.push({
+      name,
+      HP: cleanNumber(data[5]),
+      STR: cleanNumber(data[6]),
+      ATS: cleanNumber(data[7]),
+      DEF: cleanNumber(data[8]),
+      ADF: cleanNumber(data[9]),
+      SKILL: cleanNumber(data[10]),
+      img: tmp[0],
+      url: tmp[1],
+      hero: data[2]
+        ? {
+            name: heroArr[0].split('.')[0],
+            img: heroArr[1],
+            url: heroArr[2],
+          }
+        : null,
+      effect: data[4],
+      level: tr.dataset.param2 as LEVEL,
+      bg: tmp[2],
+    });
+  });
+
+  return weaponDatas;
+};
+
 /**
  * 获取武器数据
  */
@@ -44,85 +116,7 @@ export async function getWeapons() {
     }
 
     return await element
-      .$$eval(
-        `tr.divsort.itemhover`,
-        (trs, separator) => {
-          function cleanNumber(data: string) {
-            const result = data.split('/').at(-1);
-            return result ? +result : 0;
-          }
-
-          const weaponDatas: Weapon[] = [];
-          trs.forEach((tr) => {
-            const tds =
-              tr.querySelectorAll<HTMLTableCellElement>(`td:not(td td)`);
-            const data = Array.from(tds).map((td, index) => {
-              if (td == null) return '';
-
-              switch (index) {
-                case 0:
-                  const avatar = td.querySelector(
-                    'div.equip_img img',
-                  ) as HTMLImageElement;
-                  const aTag = td.querySelector(
-                    'div.equip_name a',
-                  ) as HTMLAnchorElement;
-                  const bg = td.querySelector(
-                    'div.equip_lv img',
-                  ) as HTMLImageElement;
-                  return avatar
-                    ? `${avatar.src}${separator}${aTag.href}${separator}${bg.src}`
-                    : '';
-                case 2:
-                  const img = td.querySelector('img');
-                  const a = td.querySelector('a');
-                  if (img == null || a == null) return '';
-                  return `${img.alt.split(' ').at(-1)}${separator}${
-                    img.src
-                  }${separator}${a.href}`;
-                case 4:
-                  return td.innerHTML
-                    .replace(/\<br\>/g, '/n')
-                    .replace(/\<.*?\>/g, '');
-
-                default:
-                  break;
-              }
-
-              return td.innerText;
-            });
-
-            const name = data[1].split('.')[0];
-            const heroArr = data[2].split(separator);
-            if (!name) return;
-            const tmp = data[0].split(separator);
-            weaponDatas.push({
-              name,
-              HP: cleanNumber(data[5]),
-              STR: cleanNumber(data[6]),
-              ATS: cleanNumber(data[7]),
-              DEF: cleanNumber(data[8]),
-              ADF: cleanNumber(data[9]),
-              SKILL: cleanNumber(data[10]),
-              img: tmp[0],
-              url: tmp[1],
-              hero: data[2]
-                ? {
-                    name: heroArr[0].split('.')[0],
-                    img: heroArr[1],
-                    url: heroArr[2],
-                  }
-                : null,
-              effect: data[4],
-              level: tr.dataset.param2 as LEVEL,
-              bg: tmp[2],
-            });
-          });
-
-          return weaponDatas;
-        },
-        separator,
-      )
+      .$$eval(`tr.divsort.itemhover`, opera, separator)
       .then((data) => {
         console.log(`>>> 武器数据获取成功.`);
         return data;
